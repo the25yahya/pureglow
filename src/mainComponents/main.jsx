9be50590c3e986a367f7cb8418d840452698cdab
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-08-30 03:53:05 +0200

    added main

--- src/mainComponents/main.jsx
+++ src/mainComponents/main.jsx
@@ -1,12 +1,51 @@
 import '../mainComponentsCss/main.css'
+import star from '../imgs/star.png'
 
 
+
+function Product(props){
+    return(
+        <div className='product'>
+         <div className='product-description'>
+            <h1>
+                Body Cream
+            </h1>
+            <p>
+                Caring about the body with creams that typically contain the highest porpotion pf all and natural fats and are great for dry skin, and areas like the elbows, knees and heels.
+            </p>
+         </div>
+         <div>
+          <p>{props.rating}</p>
+          <div className='stars'>
+            <img src={star}/>
+            <img src={star}/>
+            <img src={star}/>
+            <img src={star}/>
+            <img src={star}/>
+          </div>
+          <div>
+            <p>{props.price}</p>
+            <p>{props.weight}</p>
+          </div>
+         </div>
+        </div>
+    )
+}
+////////////////////////
 function Main(){
     return(
      <div className='main'>
-        main
+        <Product
+            rating="best rated"
+            price="$150"
+            weight="0.5kg"
+        />
+        <img
+            src='https://vault.uicore.co/spa/wp-content/uploads/sites/18/2019/01/product-3.webp'
+        />
      </div>
     )
 }
+///////////////////////
 
 export default Main;
