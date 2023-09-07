aacee7dfba46febd3df76b88479a650aedf30667
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-09-07 03:52:38 +0200

    added product component

--- src/mainComponents/product.jsx
+++ src/mainComponents/product.jsx
@@ -1,9 +1,24 @@
+import PropTypes from 'prop-types';
+
+
 function Product(props){
     return(
         <div className="product2">
-         
+         <img
+            src={props.src}
+         />
+         <p>{props.name}</p>
+         <p>{props.tag}</p>
+         <p>{props.price}</p>
+         <button>QUICK BUY</button>
         </div>
     )
 }
+Product.propTypes = {
+    src: PropTypes.string.isRequired, // Allowing string type for 'rating'
+    tag: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+  };
 
 export default Product
